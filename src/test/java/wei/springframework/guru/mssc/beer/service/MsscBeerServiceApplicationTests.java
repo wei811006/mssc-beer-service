141e65ae39bc6420a5034c2f0a817de2128aeb94
wei <vloude0605@gmail.com>
2020-06-15 20:58:21 +0800

    init

---
+++ src/test/java/wei/springframework/guru/mssc/beer/service/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package wei.springframework.guru.mssc.beer.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
